4829c6b426976ec492c0b8a4c2de74075c9d9458
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-22 14:04:38 -0500

    all /get and /post functions working

--- api/project/model.js
+++ api/project/model.js
@@ -1,4 +1,16 @@
 // build your `Project` model here
 const db = require("../../data/dbConfig");
 
-module.exports = {};
+module.exports = { getAll, postProject };
+
+function getAll() {
+  return db("projects");
+}
+
+function postProject(project) {
+  return db("projects")
+    .insert(project)
+    .then(([id]) => {
+      return db("projects").where("project_id", id);
+    });
+}
--- api/resource/model.js
+++ api/resource/model.js
@@ -1,4 +1,17 @@
 // build your `Project` model here
 const db = require("../../data/dbConfig");
 
-module.exports = {};
+module.exports = { getAll, postResource };
+
+function getAll() {
+  return db("resources");
+}
+
+function postResource(resource) {
+  // code
+  return db("resources")
+    .insert(resource)
+    .then(([id]) => {
+      return db("resources").where("resource_id", id);
+    });
+}
