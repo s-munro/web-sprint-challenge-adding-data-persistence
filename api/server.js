babc1152789997f4978766eefc71d3c55fc795fd
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-22 13:10:14 -0500

    scaffolding

--- api/server.js
+++ api/server.js
@@ -1,1 +1,10 @@
 // build your server here
+const express = require("express");
+
+const db = require("../data/dbConfig");
+
+const server = express();
+
+server.use(express.json());
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+const server = require("./api/server");
+
+const PORT = process.env.PORT || 7000;
+
+server.listen(PORT, () => {
+  console.log(`magic happening on port ${PORT}`);
+});
